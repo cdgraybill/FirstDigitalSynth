c5f1e0db3f9554cc9eaf91c89f138b58f0c10cc0
Cameron Graybill <cdgraybill@gmail.com>
2020-05-07 19:36:48 -0500

    Add project files.

---
+++ Synth/Synth.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Synth
+{
+    partial class Synth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Synth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 418);
+            this.KeyPreview = true;
+            this.Name = "Synth";
+            this.Text = "Synth";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Synth_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
